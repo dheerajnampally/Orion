8761124e0d5875282c54ce68f1aca3d9d8483a03
Dheeraj Nampally <nampally@motivationfactory.com>
2022-02-22 15:26:12 +0100

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Orion.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
