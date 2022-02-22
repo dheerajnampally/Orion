474e224eac6b4f8121d8c546f8b86844d71cf8f3
Dheeraj Nampally <nampally@motivationfactory.com>
2022-02-22 17:01:16 +0100

    Identity Login

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -1,13 +1,28 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using Orion.Models;
 
 namespace Orion.Data
 {
-    public class ApplicationDbContext : IdentityDbContext
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<ApplicationUser>()
+                .Property(e => e.FirstName)
+                .HasMaxLength(250);
+
+            modelBuilder.Entity<ApplicationUser>()
+                .Property(e => e.LastName)
+                .HasMaxLength(250);
+
         }
     }
 }
